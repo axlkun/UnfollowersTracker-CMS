0b140eb2d15b055ff98fb2e6b3e51210f4551ecb
axlkun <axelcruz.dev@gmail.com>
2023-11-19 00:52:00 -0600

    add user data to dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Usuario;
 
 class DashboardController extends Controller
 {
@@ -15,15 +16,19 @@ class DashboardController extends Controller
     {
         $categoriesCount = Category::count();
         $articlesCount = Article::count();
+        $usersCount = Usuario::count();
 
         $latestCategory = Category::latest('created_at')->value('created_at') ? Category::latest('created_at')->value('created_at')->diffForHumans() : 'No records available';
         $latestArticle = Article::latest('created_at')->value('created_at') ? Article::latest('created_at')->value('created_at')->diffForHumans() : 'No records available';
+        $latestUser = Usuario::latest('created_at')->value('created_at') ? Usuario::latest('created_at')->value('created_at')->diffForHumans() : 'No records available';
 
         return Inertia::render('Dashboard', [
             'categoriesCount' => $categoriesCount,
             'articlesCount' => $articlesCount,
+            'usersCount' => $usersCount,
             'latestCategory' => $latestCategory,
             'latestArticle' => $latestArticle,
+            'latestUser' => $latestUser,
         ]);
     }
 }
