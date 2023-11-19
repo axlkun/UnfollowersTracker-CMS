04f18921c65e519229fd8b614e05759f2bd09094
axlkun <axelcruz.dev@gmail.com>
2023-11-18 23:06:39 -0600

    create tables for api

--- database/migrations/2023_11_12_202616_create_api_data_table.php
+++ database/migrations/2023_11_12_202616_create_api_data_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('api_data', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('usuario_id'); // Llave foránea
+            $table->json('close_friends')->nullable();
+            $table->json('followers')->nullable();
+            $table->json('following')->nullable();
+            $table->json('hide_story_from')->nullable();
+            $table->json('pending_follow_requests')->nullable();
+            $table->json('recent_follow_requests')->nullable();
+            $table->json('recently_unfollowed_accounts')->nullable();
+            $table->json('removed_suggestions')->nullable();
             $table->timestamps();
+
+            // Definir la restricción de clave foránea
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
         });
     }
 
