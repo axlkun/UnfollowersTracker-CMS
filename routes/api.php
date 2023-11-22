772bee901980dd59f9a5c1c3aceedace0f7307df
axlkun <axelcruz.dev@gmail.com>
2023-11-22 11:58:46 -0600

    endpoint to upload image from ckeditor

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('articles',[ArticlesController::class,'index']);
 Route::get('articles/{article:slug}',[ArticlesController::class,'show']);
 Route::get('related-articles/{article:slug}',[ArticlesController::class,'relatedArticles']);
+Route::post('image-upload',[ArticlesController::class,'storeImage']);
 
 Route::get('settings',[SettingsController::class,'index']);
 
