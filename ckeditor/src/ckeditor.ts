31b3e268194a4716baa7d46905ea83ba9dcef9c5
axlkun <axelcruz.dev@gmail.com>
2023-11-22 15:03:52 -0600

    ckeditor custom build

---
+++ ckeditor/src/ckeditor.ts
@@ -0,0 +1,134 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import {
+	Image,
+	ImageCaption,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
+import {
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar
+} from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		AutoLink,
+		Autoformat,
+		BlockQuote,
+		Bold,
+		Essentials,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		Heading,
+		Image,
+		ImageCaption,
+		ImageResize,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		List,
+		ListProperties,
+		MediaEmbed,
+		Paragraph,
+		RemoveFormat,
+		ShowBlocks,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableProperties,
+		TableToolbar,
+		TextTransformation,
+		WordCount
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'heading',
+				'bold',
+				'fontBackgroundColor',
+				'fontColor',
+				'fontSize',
+				'fontFamily',
+				'removeFormat',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'alignment',
+				'|',
+				'outdent',
+				'indent',
+				'|',
+				'imageUpload',
+				'blockQuote',
+				'insertTable',
+				'mediaEmbed',
+				'showBlocks',
+				'undo',
+				'redo'
+			]
+		},
+		language: 'en',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells',
+				'tableCellProperties',
+				'tableProperties'
+			]
+		}
+	};
+}
+
+export default Editor;
