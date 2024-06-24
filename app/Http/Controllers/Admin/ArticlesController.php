1cffb95c4ed124c15c1942a1978b28eb97008bba
axlkun <axelcruz.dev@gmail.com>
2024-06-23 21:19:59 -0600

    feat: return only required data

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Actions\UploadFile;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
+use App\Http\Resources\ArticleResourceIndex;
 use App\Http\Resources\CategoryResource;
 use App\Models\Article;
 use App\Models\Category;
@@ -19,7 +20,7 @@ class ArticlesController extends Controller
         $articles = Article::with('categories')->latest()->simplePaginate(10);
 
         return Inertia::render('Articles/Index', [
-            'articles' => ArticleResource::collection($articles)
+            'articles' => ArticleResourceIndex::collection($articles)
         ]);
     }
 
